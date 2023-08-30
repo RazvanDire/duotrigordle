443eb4d846a6c4761c6aaba93890d63ed81c69db
RazvanDire <costeastefan2003@gmail.com>
2023-08-30 14:30:01 +0300

    Fixed the layout of the hotbar
    Signed-off-by: RazvanDire <costeastefan2003@gmail.com>

--- src/components/guess_count.tsx
+++ src/components/guess_count.tsx
@@ -7,16 +7,15 @@ export default function GuessCount({
 }) {
   let sign = "+";
   let color = "text-white";
-  
+
   let remainingGuesses = 5 - guesses + gamesWon;
   if (remainingGuesses <= 0) sign = "";
   if (remainingGuesses < 0) color = "text-[#f23636]";
 
   return (
-    <div className={`${color} flex-col`}>
-      <div className="text-white">Boards: {gamesWon}/32</div>
+    <span className={`${color}`}>
       Guesses: {guesses}/37 ({sign}
       {remainingGuesses})
-    </div>
+    </span>
   );
 }
--- src/components/hotbar.tsx
+++ src/components/hotbar.tsx
@@ -3,7 +3,8 @@ import GuessCount from "./guess_count";
 
 export default function Hotbar({ gameState }: { gameState: GameState }) {
   return (
-    <div className="flex items-end flex-col">
+    <div className="flex items-end flex-row justify-between">
+      <div className="text-white">Boards: {gameState.gamesWon}/32</div>
       <GuessCount guesses={gameState.guesses} gamesWon={gameState.gamesWon} />
     </div>
   );
