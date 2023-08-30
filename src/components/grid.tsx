3eba05f732b58be2d3a702e4840642a5485fc0bd
RazvanDire <costeastefan2003@gmail.com>
2023-08-30 21:19:49 +0300

    Change grid layout depending on screen size
    Signed-off-by: RazvanDire <costeastefan2003@gmail.com>

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -3,7 +3,7 @@ import Board from "./board";
 
 export default function Grid({ boards }: { boards: BoardInfo[] }) {
   return (
-    <div className="grid grid-cols-4 gap-4 h-5/6 overflow-y-scroll">
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4 h-5/6 overflow-y-scroll">
       {boards.map((board, index) => (
         <Board board={board} key={index} />
       ))}
