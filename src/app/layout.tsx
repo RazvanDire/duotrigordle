7f599923ed8822750993ec719746fec14568796d
RazvanDire <costeastefan2003@gmail.com>
2023-09-02 18:42:53 +0300

    Test iOS screen size fix
    Signed-off-by: RazvanDire <costeastefan2003@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`h-screen w-screen ${inter.className}`}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={`h-full w-screen ${inter.className}`}>{children}</body>
     </html>
   );
 }
